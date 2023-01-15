a87e80fb2c1757e77a8da82aa5ee1a0b97c7f184
Kirill Golubjov <kirillcnz@gmail.com>
2023-01-15 11:30:47 +0200

    Implementing Error and Success Messages

--- forkify-app/src/js/controller.js
+++ forkify-app/src/js/controller.js
@@ -24,7 +24,7 @@ const controlRecepes = async function () {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    console.log(err);
+    recipeView.renderError();
   }
 };
 
